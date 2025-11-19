b5a8c3a932607c5a23e8e359339b7f5f63389d0e
Maxime Amodei <maxime.amodei@uliege.be>
2025-11-19 14:50:26 +0100

    feat: extract stdout/err from valis in outputs

--- script.py
+++ script.py
@@ -1,4 +1,26 @@
+import pathlib
+import sys
+from contextlib import contextmanager
+from io import StringIO
+
 from app_engine_valis_experiment.io_utils import find_inputs
 from app_engine_valis_experiment.register import register
 
-register(find_inputs())
+
+@contextmanager
+def replace_out_err():
+    bkp = sys.stdout, sys.stderr
+    o, e = StringIO(), StringIO()
+    sys.stdout, sys.stderr = o, e
+    try:
+        yield o, e
+    finally:
+        sys.stdout, sys.stderr = bkp
+
+
+inputs = find_inputs()
+with replace_out_err() as (stdout, stderr):
+    register(inputs)
+
+pathlib.Path("/outputs/valis_stdout").write_text(stdout.getvalue())
+pathlib.Path("/outputs/valis_stderr").write_text(stderr.getvalue())
